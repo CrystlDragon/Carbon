9f04021e4a02c3a2b7e060b9e3dfb4fcb1dbe3d2
Hold My Beer <39173733+CrystlDragon@users.noreply.github.com>
2020-07-16 18:10:32 -0400

    finished textures for blocks/items

---
+++ main/java/com/bloodinthebreeze/carbon/constructors/blocks/CokeBlock.java
@@ -0,0 +1,21 @@
+package com.bloodinthebreeze.carbon.constructors.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+
+public class CokeBlock extends Block
+{
+    public CokeBlock()
+    {
+        super
+                (
+                        net.minecraft.block.Block.Properties.create(Material.IRON)
+                                .hardnessAndResistance(2.0f, 3.0f)
+                                .sound(SoundType.METAL)
+                                .harvestLevel(0)
+                                .harvestTool(ToolType.PICKAXE)
+                );
+    }
+}
--- main/java/com/bloodinthebreeze/carbon/util/RegistryHandle.java
+++ main/java/com/bloodinthebreeze/carbon/util/RegistryHandle.java
@@ -32,6 +32,8 @@ public class RegistryHandle
             public static final RegistryObject<Item> GRAPHENE = ITEMS.register("graphene", ItemBase::new);
             public static final RegistryObject<Item> DIAMANE = ITEMS.register("diamane", ItemBase::new);
             public static final RegistryObject<Item> LONSDALEITE = ITEMS.register("lonsdaleite", ItemBase::new);
+            public static final RegistryObject<Item> ACTIVATED_CARBON = ITEMS.register("activated_carbon", ItemBase::new);
+
         //without blocks
             public static final RegistryObject<Item> DIAMOND_HARDENED = ITEMS.register("hardened_diamond", ItemBase::new);
             public static final RegistryObject<Item> LOW_CARBON_STEEL_INGOT = ITEMS.register("low_carbon_steel_ingot", ItemBase::new);
@@ -51,6 +53,8 @@ public class RegistryHandle
         public static final RegistryObject<Block> SILICON_BLOCK = BLOCKS.register("silicon_block", SiliconBlock::new);
         public static final RegistryObject<Block> SILICON_CARBIDE_BLOCK = BLOCKS.register("silicon_carbide_block", SiliconCarbideBlock::new);
         public static final RegistryObject<Block> TUNGSTEN_CARBIDE_BLOCK = BLOCKS.register("tungsten_carbide_block", TungstenCarbideBlock::new);
+        public static final RegistryObject<Block> COKE_BLOCK = BLOCKS.register("coke_block", CokeBlock::new);
+        public static final RegistryObject<Block> ACTIVATED_CARBON_BLOCK = BLOCKS.register("activated_carbon_block", ActivatedCarbonBlock::new);
 
 
     //block items
@@ -62,4 +66,7 @@ public class RegistryHandle
         public static final RegistryObject<Item> HIGH_CARBON_STEEL_BLOCK_ITEM = ITEMS.register("high_carbon_steel_block", () -> new BlockItemBase(HIGH_CARBON_STEEL_BLOCK.get()));
         public static final RegistryObject<Item> TUNGSTEN_BLOCK_ITEM = ITEMS.register("tungsten_block", () -> new BlockItemBase(TUNGSTEN_BLOCK.get()));
         public static final RegistryObject<Item> TUNGSTEN_CARBIDE_BLOCK_ITEM = ITEMS.register("tungsten_carbide_block", () -> new BlockItemBase(TUNGSTEN_CARBIDE_BLOCK.get()));
+        public static final RegistryObject<Item> COKE_BLOCK_ITEM = ITEMS.register("coke_block", () -> new BlockItemBase(COKE_BLOCK.get()));
+        public static final RegistryObject<Item> ACTIVATED_CARBON_BLOCK_ITEM = ITEMS.register("activated_carbon_block", () -> new BlockItemBase(ACTIVATED_CARBON_BLOCK.get()));
+
 }
